9a976b6fa9899cdd9ea25b536b7bcfcba94914bf
zcq <zcq960921@163.com>
2019-11-04 23:28:53 +0800

    typeAlias别名设置

--- src/main/java/com/zcq/po/Role.java
+++ src/main/java/com/zcq/po/Role.java
@@ -1,12 +1,14 @@
 package com.zcq.po;
 
 import lombok.Data;
+import org.apache.ibatis.type.Alias;
 
 /**
  * @Author: chaoqun
  * @Date: 2019/11/4 20:34
  */
 @Data
+@Alias("role")
 public class Role {
     private Long id;
 
